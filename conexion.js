0d915a544ebd7fb69035c0105434e47047eca6ec
Esteban <esteban.icc@gmail.com>
2019-11-14 15:47:35 -0500

    comit 2

--- conexion.js
+++ conexion.js
@@ -1,16 +1,56 @@
 
 const fs = require('fs');
-let extraerdatos = (path) => {
-    return new Promise((resolve,reject)=>{
-        fs.readFile('API_IT.CEL.SETS_DS2_es_csv_v2_485339.csv', 'utf8', function (err, data) {
-            var dataArray2 = data.split(/\r?\n/)
-            if(Number(path)){
-                reject("error")
-            }
-            resolve(dataArray2);
-           });
-    });
-    
+function agregar(path){
+        
+          console.log(path)
+           };
+let leercsv = (path) =>{
+            return new Promise((resolve, reject) =>{
+                fs.readFile(path, 'utf8', function (err, data) {
+                    
+                    if (err){
+                        reject(err);
+                    } else {
+                        dataArray2 = data.split(/\r?\n/)
+                        resolve(dataArray2);
+                    }
+                  });
+            });
+        };
+let generardatos = (datos,año,pais) =>{
+            return new Promise((resolve, reject) =>{
+                console.log(datos[4][3])
+                let dist=datos[4]
+                console.log(dist.length)
+                console.log(pais)
+                
+                for(let i=0; i<=dist.length; i++){
+                    if(año == datos[4][i]){
+                        let pos=i
+                    }
+                }
+                for(let j=0; j<=datos.length; j++){
+                    if(pais == datos[j][4]){
+                        let pos2=j
+                    }
+                }
+                console.log(pos,pos2)
+                resolve("funciono")
+            });
+        };
+
+
+leercsv("API_IT.CEL.SETS_DS2_es_csv_v2_485339.csv")
+.then(archivo =>{
+    generardatos(archivo,1999,"ALB").then(archivo2 =>{
+        console.log(archivo2)
+    }).catch((err)=> console.log("error: ",err))
+}).catch((err)=> console.log("error: ",err))
+
+
+module.exports = {
+	leercsv: leercsv,
+	generardatos: generardatos
 }
-var datos=extraerdatos("hola").then(datos)
- console.log(datos)
+
+
