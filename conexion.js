319a60ef3f3d76a7d35395e08ed79cff3876dbea
Esteban <esteban.icc@gmail.com>
2019-11-13 22:49:34 -0500

    comit2 conexion csv

--- conexion.js
+++ conexion.js
@@ -1,1 +1,16 @@
-console.log("hola")
+
+const fs = require('fs');
+let extraerdatos = (path) => {
+    return new Promise((resolve,reject)=>{
+        fs.readFile('API_IT.CEL.SETS_DS2_es_csv_v2_485339.csv', 'utf8', function (err, data) {
+            var dataArray2 = data.split(/\r?\n/)
+            if(Number(path)){
+                reject("error")
+            }
+            resolve(dataArray2);
+           });
+    });
+    
+}
+var datos=extraerdatos("hola").then(datos)
+ console.log(datos)
