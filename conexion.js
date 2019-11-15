4f9e7e8e0621cfd810c8c627ac89e0c6edc5a0af
esteban <esteban.icc@gmail.com>
2019-11-15 02:00:36 -0500

    conexion final

--- conexion.js
+++ conexion.js
@@ -1,9 +1,6 @@
 
 const fs = require('fs');
-function agregar(path){
-        
-          console.log(path)
-           };
+
 let leercsv = (path) =>{
             return new Promise((resolve, reject) =>{
                 fs.readFile(path, 'utf8', function (err, data) {
@@ -19,30 +16,100 @@ let leercsv = (path) =>{
         };
 let generardatos = (datos,año,pais) =>{
             return new Promise((resolve, reject) =>{
-                console.log(datos[4][3])
-                let dist=datos[4]
-                console.log(dist.length)
-                console.log(pais)
                 
-                for(let i=0; i<=dist.length; i++){
-                    if(año == datos[4][i]){
-                        let pos=i
+                var dist=datos[4].split(",")
+                
+                var pos1=0
+                for (var i=0; i < dist.length; i++) {
+                    if (parseInt(dist[i].substring(2,6), 10)==año){
+                        
+                        var pos1=i
                     }
-                }
-                for(let j=0; j<=datos.length; j++){
-                    if(pais == datos[j][4]){
-                        let pos2=j
+                 }
+                 limite=33
+                 let data = '';
+                 for (var i=4; i < datos.length; i++) {
+                    try{
+                    var datos2=datos[i].split(",")
+                    var datos3=datos2[1].substring(2,5)
+                    if (datos3==pais){
+                        
+                        limit2=datos2[pos1].length -2
+                        var numero=datos2[pos1].substring(2,limit2)
+                        data += "Nombre pais:\t"+datos2[0].substring(1,10)+"\n";
+                        data +="Numeros de subscripciones:\t"+datos2[pos1].substring(2,limit2)+"\n";
                     }
+                  }
+                  catch(err){
+
+                  }
+                     
+                 }
+                 var num=0
+                 var cont=0
+                 var lis5num=[]
+                 var dicnum5={}
+                 var listop5=[]
+                 var dictop5=[]
+                 for (var i=5; i < datos.length; i++) {
+                    var datos5=datos[i].split(",")
+                    try{
+                     limite2=datos5[pos1].length-2
+                     if((!isNaN(parseInt(datos5[pos1].substring(2,limite2), 10))) && (parseInt(datos5[pos1].substring(2,limite2), 10)!=0)){
+                     num+=parseInt(datos5[pos1].substring(2,limite2), 10)
+                     cont+=1
+                     listop5.push(num)
+                     dictop5[datos5[0].substring(1,10)]=num
+                     if(numero>num){
+                         lis5num.push(num)
+                         dicnum5[datos5[0].substring(1,10)]=num
+                     }
+                    }
+                     }
+                    catch(err)
+                    {
+                        
+                    } 
+
                 }
-                console.log(pos,pos2)
-                resolve("funciono")
+                lis5num=lis5num.sort()
+                lis5num=lis5num.slice(lis5num-5)
+                listop5=listop5.sort()
+                listop5=listop5.slice(listop5.length-5)                 
+                 media=Math.round(num/cont)
+                 data+="Media de subcripciones:\t"+media+"\n"
+                 if (media > numero){
+                     data+="La media de subscripciones:\t"+media+" es mas alta que el numero de subcripcioes: \t"+numero+"\n"
+                 }
+                 else{
+                    data+="El numero de subscripciones \t"+numero+" es mas alta que la media de subscripciones: \t"+media+"\n"
+                 }
+                 data+="\nTOP 5 PAISES POR DEBAJO DE:"+numero+"\n"
+                 for (key in dicnum5) {
+                    for(var i=0; i < lis5num.length; i++){
+                        if(dicnum5[key]==lis5num[i]){
+                            data+=key+" : "+lis5num[i]+"\n"
+                        }
+                    
+                 }  
+                }
+                data+="\nTOP 5 PAISES EN EL AÑO:"+año+"\n"
+                 for (key in dictop5) {
+                    for(var i=0; i < listop5.length; i++){
+                        if(dictop5[key]==listop5[i]){
+                            data+=key+" : "+listop5[i]+"\n"
+                        }
+                    
+                 }  
+                }
+                resolve(data)
             });
         };
 
 
 leercsv("API_IT.CEL.SETS_DS2_es_csv_v2_485339.csv")
 .then(archivo =>{
-    generardatos(archivo,1999,"ALB").then(archivo2 =>{
+    generardatos(archivo,2012,"ESP").then(archivo2 =>{
         console.log(archivo2)
     }).catch((err)=> console.log("error: ",err))
 }).catch((err)=> console.log("error: ",err))
