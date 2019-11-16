018325237d5e87f2a7c5bb992bc992d1a823dfaf
Jonathan <jonathan.mc.96@hotmail.com>
2019-11-15 20:13:01 -0500

    Agregacion Pagina Web

--- app.js
+++ app.js
@@ -1,34 +1,47 @@
-const fs=require('fs');
+const fs = require('fs');
 const chalk = require('chalk');
-const {argv} = require('./config/yargs.js')
+const { argv } = require('./config/yargs.js')
 
-const{leercsv,generardatos} = require('./archivos/conexion.js');
+const { leercsv, generardatos } = require('./archivos/conexion.js');
 
 let comando = argv._[0]
 console.log(comando)
-switch(comando){
+switch (comando) {
     case 'publicar':
         leercsv(argv.file)
-        .then(archivo =>{
-            generardatos(archivo,argv.year,argv.country).then(archivo2 =>{
-            
-            console.log(chalk.blue(archivo2))
-            }).catch((err)=> console.log("error: ",err))
-        }).catch((err)=> console.log("error: ",err))
+            .then(archivo => {
+                generardatos(archivo, argv.year, argv.country).then(archivo2 => {
+                    const http = require('http');
+
+                    const hostname = '127.0.0.1';
+                    const port = 3000;
+
+                    const server = http.createServer((req, res) => {
+                        res.statusCode = 200;
+                        res.setHeader('Content-Type', 'text/plain');
+                        res.end(archivo2);
+                    });
+
+                    server.listen(port, hostname, () => {
+                        console.log(`Server running at http://${hostname}:${port}/`);
+                    });
+                    console.log(chalk.blue(archivo2))
+                }).catch((err) => console.log("error: ", err))
+            }).catch((err) => console.log("error: ", err))
         break;
     case 'guardar':
         leercsv(argv.file)
-        .then(archivo =>{
-            generardatos(archivo,argv.year,argv.country).then(archivo2 =>{
-                fs.writeFile(`./${argv.out}.txt`, `${archivo2}`, error => {
-                    if (error)
-                      console.log(error);
-                    else
-                      console.log(chalk.green('El archivo fue creado'));
-                      console.log(chalk.blue(archivo2));
-                  });
-            }).catch((err)=> console.log("error: ",err))
-        }).catch((err)=> console.log("error: ",err))
+            .then(archivo => {
+                generardatos(archivo, argv.year, argv.country).then(archivo2 => {
+                    fs.writeFile(`./${argv.out}.txt`, `${archivo2}`, error => {
+                        if (error)
+                            console.log(error);
+                        else
+                            console.log(chalk.green('El archivo fue creado'));
+                        console.log(chalk.blue(archivo2));
+                    });
+                }).catch((err) => console.log("error: ", err))
+            }).catch((err) => console.log("error: ", err))
         break;
     default:
         console.log('Comando no valido!')
