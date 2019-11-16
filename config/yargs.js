ab63d2b2fe02136ea7f76c57a9d2e43f82f29fe3
Jonathan <jonathan.mc.96@hotmail.com>
2019-11-16 13:51:12 -0500

    Aplicacion Ejecutable

--- config/yargs.js
+++ config/yargs.js
@@ -1,46 +1,46 @@
 let opt = {
-    file:{
+    file: {
         demand: true,
-        alias:`f`,
+        alias: `f`,
         description: `La ruta del archivo csv`
     },
-    country:{
-        alias:`p`,
+    country: {
+        alias: `p`,
         description: 'Permite determinar el país a analizar a través de su código'
     },
-    year:{
-        alias:`l`,
-        default: 10,
+    year: {
+        alias: `l`,
+        default: 2018,
         description: 'Permite especificar el año para el cual se requiere las estadísticas. Por defecto, 2018.'
     }
 }
 
 let optg = {
-    file:{
+    file: {
         demand: true,
-        alias:`f`,
+        alias: `f`,
         description: `La ruta del archivo csv`
     },
-    country:{
-        alias:`p`,
+    country: {
+        alias: `p`,
         description: 'Permite determinar el país a analizar a través de su código'
     },
-    year:{
-        alias:`l`,
-        default: 10,
+    year: {
+        alias: `l`,
+        default: 2018,
         description: 'Permite especificar el año para el cual se requiere las estadísticas. Por defecto, 2018.'
     },
-    out:{
-        alias:`o`,
-        description:`Establece el nombre del archivo donde se almacenrá los resultados.`
+    out: {
+        alias: `o`,
+        description: `Establece el nombre del archivo donde se almacenrá los resultados.`
     }
 }
 
 const argv = require('yargs')
-.command('publicar','Crea un archivo co la tabla de multiplicar',opt)
-.command('guardar','Guarda los datos de los paises',optg)
-.help()
-.argv;
+    .command('publicar', 'Crea un archivo co la tabla de multiplicar', opt)
+    .command('guardar', 'Guarda los datos de los paises', optg)
+    .help()
+    .argv;
 
 
 module.exports = {
